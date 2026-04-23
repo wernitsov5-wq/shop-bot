712bf14e4900d5dfb0e02680ea6098477def209d
wernitsov5-wq <wernitsov5@gmail.com>
2026-04-23 19:28:36 +0300

    Update exam_shop.py

--- exam_shop.py
+++ exam_shop.py
@@ -25,7 +25,7 @@ from telegram.ext import (
 BOT_TOKEN = "8690771289:AAEKOfGBeICFT1Rfex77-QPpxDijbBOjAms"
 MANAGER_ID = 1911945305
 
-CARD_NUMBER = "ошибка 322"
+CARD_NUMBER = "ошибка 0000 0000"
 CARD_HOLDER = "Максим"
 BANK_NAME = "Альфа Банк"
 
